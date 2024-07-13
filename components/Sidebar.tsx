5a6bfe3d284d79176fd4edc628a29db8b5b6e0e1
Sirgeeyou <bojorsergiu@gmail.com>
2024-07-13 19:33:21 +0300

    chore: sidebar icons added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,22 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 import { Separator } from "./ui/separator";
+import { Home, Library } from "lucide-react";
 
 function Sidebar({ className }: { className: string }) {
   return (
     <div className={cn(className, "border-r border-r-foreground/10")}>
       <div className="flex flex-col gap-4 p-4">
-        <span> Home</span>
+        <div className="flex gap-2">
+          <Home />
+          <span> Home</span>
+        </div>
         <span> Search placeholder</span>
         <Separator />
-        <span>Your library</span>
+        <div className="flex gap-2">
+          <Library />
+          <span>Your library</span>
+        </div>
       </div>
     </div>
   );
