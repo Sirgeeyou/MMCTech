3e3709e44b9ce7243c60c46badaab81dbb86d77a
Sirgeeyou <bojorsergiu@gmail.com>
2024-07-13 18:58:24 +0300

    fix: navbar border-b

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,11 +10,13 @@ async function Navbar() {
   const { data, error } = await supabase.auth.getUser();
 
   return (
-    <nav className="max-w-screen-lg flex w-full justify-between items-center border-b border-b-foreground/10 h-16 px-4">
-      <Link href={"/"}>Logo</Link>
-      <div className="flex gap-3 p-3 text-sm items-center">
-        <AuthButton />
-        <ToggleTheme />
+    <nav className="w-full flex justify-center border-b border-b-foreground/10">
+      <div className="max-w-screen-lg flex w-full justify-between items-center  h-16 px-4">
+        <Link href={"/"}>Logo</Link>
+        <div className="flex gap-3 p-3 text-sm items-center">
+          <AuthButton />
+          <ToggleTheme />
+        </div>
       </div>
     </nav>
   );
