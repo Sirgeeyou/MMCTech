d6d1dee47e3bce380e8a5efcd40231dea236ddc5
Sirgeeyou <bojorsergiu@gmail.com>
2024-07-16 17:27:58 +0300

    chore: added songs to album page

--- app/album/[id]/page.tsx
+++ app/album/[id]/page.tsx
@@ -6,30 +6,38 @@ export default async function page({ ...props }) {
   return (
     <main className="w-4/5 overflow-hidden flex flex-col rounded-md p-6">
       {data && (
-        <>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-4xl">{data[0]?.title}</h1>
-            <h3>
-              <p className="text-neutral-500">
-                Album by{" "}
-                <span className="text-white">
-                  {Array.isArray(data[0].artists)
-                    ? data[0].artists.map((artist) => artist.name).join(", ")
-                    : data[0].artists.name}
-                </span>
-              </p>
-            </h3>
-            <h4 className="text-md">{data[0].description}</h4>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl">{data[0]?.title}</h1>
+          <h3>
+            <p className="text-neutral-500">
+              Album by{" "}
+              <span className="text-white">
+                {Array.isArray(data[0].artists)
+                  ? data[0].artists.map((artist) => artist.name).join(", ")
+                  : data[0].artists.name}
+              </span>
+            </p>
+          </h3>
+          <h4 className="text-md">{data[0].description}</h4>
+          <div className="pt-4">
+            <h4 className="text-neutral-500 pb-2">
+              {data[0].title} contains the following songs:
+            </h4>
+            {data[0].songs && (
+              <ul>
+                {data[0].songs.map((song) => (
+                  <div
+                    className="flex justify-between hover:bg-accent items-center rounded-md px-3 my-2"
+                    key={song.id}
+                  >
+                    <li className="py-1.5">{song.title}</li>
+                    <span>{song.length.slice(0, -3)}</span>
+                  </div>
+                ))}
+              </ul>
+            )}
           </div>
-          <p className="mt-5">
-            Other albums by{" "}
-            <span>
-              {Array.isArray(data[0].artists)
-                ? data[0].artists.map((artist) => artist.name).join(", ")
-                : data[0].artists.name}{" "}
-            </span>
-          </p>
-        </>
+        </div>
       )}
     </main>
   );
--- lib/actions/getLatestArtists.ts
+++ lib/actions/getLatestArtists.ts
@@ -5,5 +5,5 @@ import { createClient } from "@/utils/supabase/server";
 export default async function getAllData() {
   const supabase = createClient();
   const { data: artists } = await supabase.from("artists").select("*");
-  return JSON.stringify(artists, null, 2);
+  return artists;
 }
